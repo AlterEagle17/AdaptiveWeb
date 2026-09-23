4380460140140ab0a56b6872c79afa6d3eab0c6b
SABARISH S <sabarishselvan63@gmail.com>
2026-09-23 07:31:06 +0530

    Create UserRepository interface for user data access

---
+++ backend/adaptiveweb/src/main/java/com/adaptiveweb/adaptiveweb/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.adaptiveweb.repository;
+
+import com.adaptiveweb.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserId(String userId);
+
+    boolean existsByUserId(String userId);
+
+    boolean existsByEmail(String email);
+}
