430791ca3f887d11f8bef227cdbbda59fae4b09d
SABARISH S <sabarishselvan63@gmail.com>
2026-09-23 07:33:29 +0530

    Implement AuthService for user registration

---
+++ backend/adaptiveweb/src/main/java/com/adaptiveweb/adaptiveweb/service/AuthService.java
@@ -0,0 +1,32 @@
+package com.adaptiveweb.service;
+
+import com.adaptiveweb.entity.User;
+import com.adaptiveweb.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User register(User user) {
+
+        if (userRepository.existsByUserId(user.getUserId())) {
+            throw new RuntimeException("User ID already exists");
+        }
+
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        return userRepository.save(user);
+    }
+}
